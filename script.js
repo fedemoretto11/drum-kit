b31ed837001cf5150d52b59407d6aca505080d6f
fedemoretto11 <fedemoretto94@gmail.com>
2023-02-02 17:45:08 -0300

    Proyecto drum-kit

---
+++ script.js
@@ -0,0 +1,23 @@
+function sonar (e) {
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+    const key = document.querySelector(`.key[data-key="${e.key}"]`)
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    console.log(key);
+    key.classList.add("playing");
+    
+}
+
+
+
+
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return
+    this.classList.remove("playing")
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener("keydown", sonar);
